6db6c24d391512192db0f4aae07295e40b795096
M4RCKIT0S <marcosicardochicote15@gmail.com>
2024-03-14 18:44:00 +0100

    Css changes and input component created

--- front-url-analyzer/src/routes/+page.svelte
+++ front-url-analyzer/src/routes/+page.svelte
@@ -1,35 +1,41 @@
 <script>
     import image from '$lib/assets/Scenes05.svg'
 
+    import Input from './Input.svelte'
+
 
     export let urls = []
     let url = ''
-    function onUrlChange({target: t}){
-        url = t.value
-    }
-    function handleClick(){
+    function handleClickUrlInput(){
+        url = url.trim()
         if(url !== ''){
             urls = [...urls, url]
         }
         url = ''
     }
+
+    let keyWord = ''
+    function handleClickKeyWord(){
+
+    }
+
 </script>
 
 
 <div class="min-h-screen flex justify-center items-center background static">
-    <div class="backdrop-blur-sm p-6 bg-white/50 rounded-md z-10 w-1/3">
+    <div class="backdrop-blur-sm p-6 bg-white/50 rounded-md z-10 w-2/4">
         <div class="flex justify-center mb-3">
-            <h1 class="text-6xl text-white">Sentiment analysis</h1>
+            <h1 class="text-6xl text-cyan-800"><strong>Sentiment Analysis</strong></h1>
         </div>
-        <div class="flex justify-center">
-            <input on:input={onUrlChange} bind:value={url} placeholder="Insert url" class="p-3 rounded-full w-100">
-            <button on:click={handleClick}>Add</button>
+        <div class="flex flex-col">
+            <Input label="Urls" placeholder="e.g. www.example.com" bind:inputValue={url} handleClick={handleClickUrlInput}/>
+            <Input label="Keyword" placeholder="e.g. Apple, Zara ..." bind:inputValue={keyWord} handleClick={handleClickKeyWord}/>
         </div>
         {#each urls as url, index}
             <p>{index+1} - {url}</p>
         {/each}
     </div>
-    <div class="absolute bottom-0 -left-10 z-0 w-3/5 h-100">
+    <div class="absolute bottom-0 -left-32 z-0 w-3/5 h-100">
         <img src={image} alt="alt" class="object-contain"/>
     </div>
 </div>
@@ -38,5 +44,8 @@
     .background{
         background: rgb(63,94,251);
         background: radial-gradient(circle, rgba(63,94,251,0.9458377100840336) 11%, rgba(70,252,227,1) 100%);
+    }
+    h1{
+        color:rgb(63,94,251)
     }
 </style>
---
+++ front-url-analyzer/src/routes/Input.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let placeholder
+    export let inputValue = ''
+    export let handleClick
+    export let label
+</script>
+
+
+<div class="flex justify-center items-center w-full p-3">
+    <p class="w-2/12 text-md text-cyan-800"><strong>{label}</strong></p>
+    <input bind:value={inputValue} placeholder={placeholder} class="p-4 rounded-l-full w-8/12 bg-white">
+    <button on:click={handleClick} class="rounded-r-full text-white rounded-l-lg p-4 text-md w-max bg-blue-500 h-4/6">Add</button>
+</div>
+
+
+<style>
+    p{
+        color:rgb(63,94,251)
+    }
+</style>
