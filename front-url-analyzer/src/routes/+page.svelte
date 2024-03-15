6fbf98a0b86fe5cfd4a45c3826934ae8286f83b0
M4RCKIT0S <marcosicardochicote15@gmail.com>
2024-03-15 10:43:37 +0100

    Css improvements and url component created

--- front-url-analyzer/src/routes/+page.svelte
+++ front-url-analyzer/src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import image from '$lib/assets/Scenes05.svg'
 
     import Input from './Input.svelte'
+    import Url from './url.svelte';
 
 
     export let urls = []
@@ -22,8 +23,8 @@
 </script>
 
 
-<div class="min-h-screen flex justify-center items-center background static">
-    <div class="backdrop-blur-sm p-6 bg-white/50 rounded-md z-10 w-2/4">
+<div class="min-h-screen flex flex-col justify-start items-center background pt-[50vh] static">
+    <div class="backdrop-blur-sm p-6 bg-white/50 rounded-md z-10 w-2/4 mt-[-25vh] mb-24 shadow-lg flex flex-col">
         <div class="flex justify-center mb-3">
             <h1 class="text-6xl text-cyan-800"><strong>Sentiment Analysis</strong></h1>
         </div>
@@ -31,9 +32,11 @@
             <Input label="Urls" placeholder="e.g. www.example.com" bind:inputValue={url} handleClick={handleClickUrlInput}/>
             <Input label="Keyword" placeholder="e.g. Apple, Zara ..." bind:inputValue={keyWord} handleClick={handleClickKeyWord}/>
         </div>
-        {#each urls as url, index}
-            <p>{index+1} - {url}</p>
-        {/each}
+        <div>
+            {#each urls as url, index}
+               <Url url={url} index={index}/>
+            {/each}
+        </div>
     </div>
     <div class="absolute bottom-0 -left-32 z-0 w-3/5 h-100">
         <img src={image} alt="alt" class="object-contain"/>
---
+++ front-url-analyzer/src/routes/url.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let url
+    export let index
+</script>
+
+
+<div class="w-full bg-white rounded-lg mt-2 p-2 shadow-md hover:scale-105 hover:bg-gray-100 transition-all duration-200 flex">
+        <p class="p-2 w-1/12">{index+1}</p>
+        <p class="grow p-2">{url}</p>
+</div>
